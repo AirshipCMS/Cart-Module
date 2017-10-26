b936afb6ffa98b28c3314594e61d16b3b71a8e12
Te Vallee <astronomemusic@gmail.com>
2017-10-26 13:36:00 -1000

    copied clear cart confirmation code from aerocart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CartService } from './cart.service';
+import { environment } from '../../environments/environment';
+
+declare var $;
 
 @Component({
   selector: 'app-cart',
@@ -12,6 +15,8 @@ export class CartComponent implements OnInit {
   cart : any;
   subtotal : number;
   quantityRange : Array<number> = [];
+  confirmClearCart : boolean;
+  dialogState : string = 'open';
 
   constructor(public cartService: CartService) {
     this.cart = this.cartService.getCart();
@@ -19,6 +24,13 @@ export class CartComponent implements OnInit {
   }
 
   ngOnInit() {
+    $('#dialog-confirm').dialog({
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+    });
+    this.toggleDialog();
   }
 
   ngDoCheck() {
@@ -29,8 +41,19 @@ export class CartComponent implements OnInit {
     this.cart.items = this.cartService.deleteCartItem(this.cart, i);
   }
 
+  toggleDialog() {
+    this.dialogState = this.dialogState === 'open' ? 'close' : 'open'
+    $('#dialog-confirm').dialog(this.dialogState);
+  }
+
   clearCart() {
     this.cart.items = this.cartService.clearCart(this.cart);
+    this.toggleDialog();
+  }
+
+  checkout() {
+    let encodedCart = encodeURIComponent(JSON.stringify((this.cart)));
+    window.location.href = `https://${environment.domain}/checkout#/#${encodedCart}`;
   }
 
 }
